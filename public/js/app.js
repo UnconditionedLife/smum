d6ebcc05a9b44e5448c2102b87a4e3733410842b
Kush Jain <kjain14@jainfamily.com>
2019-01-25 18:36:27 -0600

    Fixed node errors

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const app = express()
-const port = 3000
-app.use(express.static(__dirname + '/public/'));
-app.get('/', function(request, response){
-    response.sendFile('index.html', { root: __dirname });
-});
-app.listen(port, () => console.log(`SMUM webapp listening on port ${port}!`))
 // SMUM CHECKIN APP
 // This project is thanks to contributions of people like Kush Jain.
 // FUNCTION naming covention Prefix:
