f670ff42b63cecf89f46e822983e407f80c9bf76
kjain14 <kjain14@utexas.edu>
2020-06-06 11:59:39 -0700

    updating server root to be root of repository

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/'));
 app.get('/', function(request, response){
     response.sendFile('index.html', { root: __dirname });
 });
