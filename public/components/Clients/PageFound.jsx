f1595c697c1b8ee7ba8d32b0d5bab9f64ad1c022
UnconditionedLife <jleal67@gmail.com>
2020-08-05 20:06:18 -0700

    Revert "Using refs to define selector for jquery function"
    This reverts commit 8ebc3972733ba8d921a66d1a81e42b83ee3977bb.

--- public/components/Clients/PageFound.jsx
+++ public/components/Clients/PageFound.jsx
@@ -4,41 +4,42 @@ export default function FoundPage(props) {
     const clientData = props.clientData
     const counter = props.counter
     const columns = ["clientId","givenName","familyName","dob","street"]
-    const clientDataDiv = useRef(null);
 
-    // const DidMount = props => {
-    //     const didMountRef = useRef(false)
+    const DidMount = props => {
+        const didMountRef = useRef(false)
         
-    //     console.log("MOUNT")
-    //     useEffect(() => {
-    //         console.log("EFFECT")
-    //         if (clientData !== null) {
-    //             if (didMountRef.current) {
-    //                 const columns = ["clientId","givenName","familyName","dob","street"]
-    //                 window.uiGenSelectHTMLTable("FoundClientsContainer", clientData, columns,'clientTable')
-    //             } else didMountRef.current = true
-    //         }
-    //     })
-    // };
+        console.log("MOUNT")
+        useEffect(() => {
+            console.log("EFFECT")
+            if (clientData !== null) {
+                if (didMountRef.current) {
+                    const columns = ["clientId","givenName","familyName","dob","street"]
+                    window.uiGenSelectHTMLTable("FoundClientsContainer", clientData, columns,'clientTable')
+                } else didMountRef.current = true
+            }
+        })
+    };
 
     // I TRIED THIS AS WELL AND IT DID NOT WORK EITHER
-    useEffect(() => {
-        console.log("EFFECT")
-        console.log(clientData)
-        if (clientData !== null) {
-            console.log("IN SHOW TABLE")
-            const columns = ["clientId","givenName","familyName","dob","street"]
-            window.uiGenSelectHTMLTable(clientDataDiv.current, clientData, columns,'clientTable')
-        }
-    })
+    // useEffect(() => {
+    //     return () => {
+    //         console.log("EFFECT")
+    //         console.log(clientData)
+    //         if (clientData !== null) {
+    //             console.log("IN SHOW TABLE")
+    //             const columns = ["clientId","givenName","familyName","dob","street"]
+    //             window.uiGenSelectHTMLTable("FoundClientsContainer", clientData, columns,'clientTable')
+    //         }
+    //     }
+    // })
 
-    // DidMount()
+    DidMount()
     
     return (
         <div>
             <div><br/><br/></div>
     <div>FOUND PAGE { counter} </div>
-                <div ref={clientDataDiv} id ="FoundClientsContainer"></div>
+                <div id ="FoundClientsContainer"></div>
             <div><br/><br/></div>
         </div>
     );
