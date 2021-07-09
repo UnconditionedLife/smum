91f5686c9bfbac7442342392da3df40ad02ae65c
Scott Rotondo <rotondo@gmx.com>
2021-07-08 18:13:33 -0700

    Fix margin around cards on user profile page

--- public/components/User/UserMain.jsx
+++ public/components/User/UserMain.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Container, CardContent, CardHeader } from '@material-ui/core';
+import { Box, Container, CardContent, CardHeader } from '@material-ui/core';
 import { Card } from '../System';
 import { UserForm, PasswordForm } from '../User';
 import { getSession } from '../System/js/Database';
 
-UserMain.propTypes = {
-
-}
-
-export default function UserMain(props) {
+export default function UserMain() {
     return (
-        <Container maxWidth='md'>
-            <Card>
-                <CardHeader title="User Profile" />
-                <CardContent>
-                    <UserForm { ...props } user={ getSession().user } selfEdit />
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader title="Change Password" />
-                <CardContent>
-                    <PasswordForm { ...props } />
-                </CardContent>
-            </Card>
-        </Container>
+        <Box width='100%' p={ 2 }>
+            <Container maxWidth='md'>
+                <Card>
+                    <CardHeader title="User Profile" />
+                    <CardContent>
+                        <UserForm user={ getSession().user } selfEdit />
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader title="Change Password" />
+                    <CardContent>
+                        <PasswordForm />
+                    </CardContent>
+                </Card>
+            </Container>
+        </Box>
     )
 }
