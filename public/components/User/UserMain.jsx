2f4caf4ceb68beeda8aa2054b1cc945e3bcc16fb
Scott Rotondo <rotondo@gmx.com>
2021-09-16 17:53:43 -0700

    Retrieve current user details from database, not cache

--- public/components/User/UserMain.jsx
+++ public/components/User/UserMain.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Container, CardContent, CardHeader } from '@material-ui/core';
 import { Card } from '../System';
 import { UserForm, PasswordForm } from '../User';
-import { getSession } from '../System/js/Database';
+import { dbGetUserAsync, getSession } from '../System/js/Database';
 
 export default function UserMain() {
+    const [userObj, setUser] = useState(null);
+
+    useEffect(() => {
+        dbGetUserAsync(getSession().user.userName).then( x => { setUser(x) });
+    }, []);
+
     return (
-        <Box width='100%' p={ 2 }>
+        <Box width='100%' p={ 2 } key={ userObj }>
             <Container maxWidth='md'>
                 <Card>
                     <CardHeader title="User Profile" />
                     <CardContent>
-                        <UserForm user={ getSession().user } selfEdit />
+                        <UserForm user={ userObj } selfEdit />
                     </CardContent>
                 </Card>
                 <Card>
