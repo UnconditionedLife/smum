9495ee7996ea9d4c9f7dd0d81bcd4d01c7e5f77b
UnconditionedLife <jleal67@gmail.com>
2022-01-16 16:39:15 -0800

    Moved printer tests to DEV Database setting bar

--- public/components/Header/DbSwitch.jsx
+++ public/components/Header/DbSwitch.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Box, Switch } from '@material-ui/core';
-import { Typography } from '../System';
+import { Typography, Button } from '../System';
 import { dbSetUrl } from '../System/js/Database';
+import { prnTest } from '../System/js/Clients/Receipts';
 
 export default function DbSwitch() {
     const [switchOn, setSwitchOn] = React.useState(false);
@@ -17,6 +18,17 @@ export default function DbSwitch() {
     return (
         <Box display="flex" flexDirection="row" justifyContent="flex-end" 
          bgcolor={ switchOn ? 'warning.main' : 'gray' }>
+             <Box>
+             <Button key="print1" m={ .5 } variant="outlined" color="primary" size="small" minWidth="168px" 
+            onClick={ () => prnTest('minimal') } >
+                    Min Printer Test
+            </Button>
+            <Button key="print2" m={ .5 } variant="outlined" color="primary" size="small" minWidth="168px"
+            style = {{ marginRight: '100px' }} 
+            onClick={ () => prnTest('full') } >
+                    Full Printer Test
+            </Button>
+             </Box>
             { switchOn ? <Typography ml={ 1 } mr={ 1 }>WARNING: Production database selected</Typography> : null }
             <Typography ml={ 1 } mr={ 1 }>Database Access:</Typography>
             <Box display="flex" flexDirection="row" ml={ 1 } mr={ 1 }>
