cb0280d4b99d0555ed4b032094d3889b10f141b1
UnconditionedLife <jleal67@gmail.com>
2022-01-22 12:47:12 -0800

    Cleaned Up Index files

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express()
-const port = 3002;
-app.use(express.static(__dirname + '/'));
-app.get('/', function(request, response){
-    response.sendFile('index.html', { root: __dirname });
-});
-app.listen(port, () => console.log(`SMUM webapp listening on port ${port}!`))
