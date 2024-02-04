c45beb854b72e265b7b5464503750c4ee5e61dc1
Scott Rotondo <rotondo@gmx.com>
2024-02-03 17:41:39 -0800

    Fix indentation in Cognito.js

--- src/components/System/js/Cognito.js
+++ src/components/System/js/Cognito.js
@@ -6,19 +6,19 @@ import { getCognitoHandle } from './Database';
 import { removeErrorPrefix } from './GlobalUtils';
 
 let poolData = {
-        UserPoolId : 'us-west-2_AufYE4o3x', // Your user pool id here
-        ClientId : '7j3jhm5a3pkc67m52bf7tv10au' // Your client id here
+    UserPoolId : 'us-west-2_AufYE4o3x', // Your user pool id here
+    ClientId : '7j3jhm5a3pkc67m52bf7tv10au' // Your client id here
 };
 let userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
 
 //**** EXPORTABLE JAVASCRIPT FUNCTIONS ****
 
 export function cogSetupUser(username) {
-  let userData = {
-     Username: username,
-     Pool: userPool
-  };
-  return new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+    let userData = {
+        Username: username,
+        Pool: userPool
+    };
+    return new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
 }
 
 export async function cogCreateUserAsync(username, password, userAttributes) {
@@ -39,20 +39,20 @@ export async function cogCreateUserAsync(username, password, userAttributes) {
 }
 
 export function cogGetRefreshToken(token) {
-  return new AWSCognito.CognitoIdentityServiceProvider.CognitoRefreshToken({ RefreshToken: token })
+    return new AWSCognito.CognitoIdentityServiceProvider.CognitoRefreshToken({ RefreshToken: token })
 }
 
 export function cogSetupSession(auth) {
-  let cogAuth = {"IdToken": auth.idToken, "AccessToken": auth.accessToken}
-  return new AWSCognito.CognitoIdentityServiceProvider.CognitoUserSession(cogAuth);
+    let cogAuth = {"IdToken": auth.idToken, "AccessToken": auth.accessToken}
+    return new AWSCognito.CognitoIdentityServiceProvider.CognitoUserSession(cogAuth);
 }
 
 export function cogSetupAuthDetails(username, password) {
-  let authData = {
+    let authData = {
         Username: username,
         Password: password
-      };
-  return new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authData);
+    };
+    return new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authData);
 }
 
 export async function cogChangePasswordAsync(oldPassword, newPassword) {
