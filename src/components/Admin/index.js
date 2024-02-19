a13c35379a5cb5d00982edcb3e87fd9fde2cd411
townsag <townsag@gmail.com>
2024-02-19 12:37:05 -0800

    added routing for admin error page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -3,6 +3,7 @@ import AdminRouter from './AdminMain/AdminRouter.jsx';
 import AdminServicesPage from './AdminServicesPage/AdminServicesPage.jsx';
 import AllUsersPage from './AllUsersPage/AllUsersPage.jsx';
 import CalendarPage from './SettingsPage/CalendarPage.jsx';
+import ErrorPage from './ErrorPage/ErrorPage.jsx';
 import ReportsPage from './ReportsPage/ReportsPage.jsx';
 import ServiceTypePage from './ServiceTypesPage/ServiceTypePage.jsx';
 import ServiceTypeFormDialog from './ServiceTypesPage/ServiceTypeFormDialog.jsx';
@@ -10,5 +11,5 @@ import SettingsPage from './SettingsPage/SettingsPage.jsx';
 import UserPage from './UserPage/UserPage.jsx';
 import ReportsHeader from './ReportsPage/ReportsHeader.jsx';
 
-export  { AdminMain, AdminRouter, AdminServicesPage, AllUsersPage, CalendarPage, ReportsPage, ServiceTypePage, 
+export  { AdminMain, AdminRouter, AdminServicesPage, AllUsersPage, CalendarPage, ErrorPage, ReportsPage, ServiceTypePage, 
             ServiceTypeFormDialog, SettingsPage, UserPage, ReportsHeader }
