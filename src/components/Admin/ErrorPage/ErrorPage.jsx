90336ebfa3f323dee377bb82c5a68f756f1c86ea
townsag <townsag@gmail.com>
2024-04-14 14:12:16 -0700

    changed the datetime object to dayjs from moment, added null/undefined checking to deal with empty or invalid date picker fields, added flag to remove date picker masking error

--- src/components/Admin/ErrorPage/ErrorPage.jsx
+++ src/components/Admin/ErrorPage/ErrorPage.jsx
@@ -1,94 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-// import moment from 'moment';
-import moment from 'moment-timezone';
-import { TextField } from '../../System'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { TextField } from "../../System";
 
+import {
+    Alert,
+    Box,
+    Table,
+    TableBody,
+    Button,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@mui/material";
 
-import { Alert, Box, Table, TableBody, Button,
-    TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { dbFetchErrorLogs } from "../../System/js/Database";
 
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
-import { dbFetchErrorLogs } from '../../System/js/Database';
-
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
 
 export default function ErrorPage(props) {
     // error messages stores the dates in utc time
-
-    const [startDate, setStartDate] = useState(moment(null));
-    const [endDate, setEndDate] = useState(moment(null));
-    const base_url = "https://hjfje6icwa.execute-api.us-west-2.amazonaws.com/";
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    // const base_url = "https://hjfje6icwa.execute-api.us-west-2.amazonaws.com/";
 
     const updateErrorRange = () => {
-        if (startDate.isValid() && endDate.isValid()) {
-            // user selects a time in local time, creating a moment object in local time
-            // the user selected time is converted to utc time before being sent to the db
-            dbFetchErrorLogs(startDate.utc().format(), moment(endDate).add(23,'h').add(59,'m').utc().format())
-            .then(newErrorMessages => {
-                props.errorMessagesUpdate(newErrorMessages);
-                props.countUpdate(newErrorMessages.length);
-            });
-        } else {
-            dbFetchErrorLogs("","").then(newErrorMessages => {
-                props.errorMessagesUpdate(newErrorMessages);
-                props.countUpdate(newErrorMessages.length);
-            });
+        let start = "";
+        let end = "";
+        if (startDate?.isValid() && endDate?.isValid()) {
+            start = startDate.utc().format();
+            end = dayjs(endDate).add(23, "h").add(59, "m").utc().format();
+        } else if (startDate?.isValid()) {
+            start = startDate.utc().format();
+        } else if (endDate?.isValid()) {
+            end = dayjs(endDate).add(23, "h").add(59, "m").utc().format();
         }
-    }  
+        dbFetchErrorLogs(start, end).then((newErrorMessages) => {
+            props.errorMessagesUpdate(newErrorMessages);
+            props.countUpdate(newErrorMessages.length);
+        });
+    };
 
-    
     return (
-        <Box width='95%' mx={ 2 }>
-            <Box display="flex" flexDirection="row" flexWrap="wrap" style={{ marginTop: '15px' }}>
-                <LocalizationProvider dateAdapter={ AdapterMoment }>
-                    <DatePicker 
-                        inputProps={{style: { paddingTop: '10px', paddingBottom:'10px'}}}  
-                        width={ 240 } 
-                        m={ 0 } 
-                        size='small' 
+        <Box width="95%" mx={2}>
+            <Box display="flex" flexDirection="row" flexWrap="wrap" style={{ marginTop: "15px" }}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        inputProps={{
+                            style: {
+                                paddingTop: "10px",
+                                paddingBottom: "10px",
+                            },
+                        }}
+                        width={240}
+                        m={0}
+                        size="small"
                         InputLabelProps={{ shrink: true }}
                         inputFormat="MMM DD YYYY"
                         label="Start Date"
                         value={startDate}
                         renderInput={(params) => <TextField {...params} />}
                         onChange={(newValue) => setStartDate(newValue)}
+                        disableMaskedInput
                     />
                     <DatePicker
-                        inputProps={{style: { paddingTop: '10px', paddingBottom:'10px'}}}  
-                        width={ 240 } 
-                        m={ 0 } 
-                        size='small' 
+                        inputProps={{
+                            style: {
+                                paddingTop: "10px",
+                                paddingBottom: "10px",
+                            },
+                        }}
+                        width={240}
+                        m={0}
+                        size="small"
                         InputLabelProps={{ shrink: true }}
                         inputFormat="MMM DD YYYY"
                         label="End Date"
                         value={endDate}
                         renderInput={(params) => <TextField {...params} />}
                         onChange={(newValue) => setEndDate(newValue)}
+                        disableMaskedInput
                     />
                 </LocalizationProvider>
-                <Button onClick={updateErrorRange} variant="contained" color="primary">Apply</Button>
+                <Button onClick={updateErrorRange} variant="contained" color="primary" >Apply</Button>
             </Box>
-
-            <TableContainer> 
+            {/* <p>start datetime: {startDate?.utc().format()}</p>
+            <p>
+                end datetime:{" "}
+                {endDate?.add(23, "h").add(59, "m").utc().format()}
+            </p> */}
+            <TableContainer>
                 <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center">Time Stamp</TableCell>
-                        <TableCell align="center">Message</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.errorMessages.map((row) => (
-                    <TableRow key={ row.logID }>
-                        <TableCell component="th" scope="row">
-                            {moment(row.logTimestamp).local().format("dddd, MMMM D, YYYY, h:mm:ss a")}
-                        </TableCell>
-                        <TableCell component="th" scope="row">{row.message}</TableCell>
-                    </TableRow>
-                    ))}
-                </TableBody>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Time Stamp</TableCell>
+                            <TableCell align="center">Message</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.errorMessages.map((row) => (
+                            <TableRow key={row.logID}>
+                                <TableCell component="th" scope="row">
+                                    {dayjs(row.logTimestamp).local().format("dddd, MMMM D, YYYY, h:mm:ss a")}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {row.message}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
                 </Table>
             </TableContainer>
             {props.errorMessages.length == 0 && (
